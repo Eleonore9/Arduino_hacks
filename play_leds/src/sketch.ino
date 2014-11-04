18002638b62d78b01d485cbd9ff53b5ab90d9b34
Eleonore <eleonore.mayola@gmail.com>
2014-11-04 17:05:41 +0000

    added the simple hello world sketch to play with LEDs and a new one to look for wifi networks

---
+++ play_leds/src/sketch.ino
@@ -0,0 +1,21 @@
+#define LED_PIN12 12
+#define LED_PIN13 13
+
+# I just alternate lighting two LEDs!
+void setup()
+{
+    pinMode(LED_PIN12, OUTPUT);
+    pinMode(LED_PIN13, OUTPUT);
+}
+
+void loop()
+{
+    digitalWrite(LED_PIN12, HIGH);
+    delay(100);
+    digitalWrite(LED_PIN12, LOW);
+    delay(900);
+    digitalWrite(LED_PIN13, HIGH);
+    delay(100);
+    digitalWrite(LED_PIN13, LOW);
+    delay(900);
+}
