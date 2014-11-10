69fcdb094d5cdc02b6e92babdb1c943629060d62
Eleonore <eleonore.mayola@gmail.com>
2014-11-10 17:02:13 +0000

    I found a simple sketch and circuit for light detection using a led

--- led_sensor/src/sketch.ino
+++ led_sensor/src/sketch.ino
@@ -1,51 +1,26 @@
 // Trying the LED light sensor from Arduino
-// webiste: http://playground.arduino.cc/Learning/LEDSensor
 
-#include <SPI.h>
+int LIGHT_SENSOR = 0;
+int LED = 13;
 
-#define LED_N_SIDE 2
-#define LED_P_SIDE 3
+int THRESHOLD = 200;
 
-void setup()
-{
-  Serial.println("Hi!");
-  Serial.begin(9600);
-  while (!Serial);
+void setup() {
+    pinMode(LIGHT_SENSOR, INPUT);
+    pinMode(LED, OUTPUT);
+    Serial.begin(9600);
 }
+void loop() {
+    Serial.println("*************");
+    int x = analogRead(LIGHT_SENSOR);
+    Serial.println(x);
+    
+    if(x < THRESHOLD) {
+        digitalWrite(LED, HIGH);
+    }    
+    else {
+        digitalWrite(LED, LOW);
+    }
 
-void loop()
-{
-  unsigned int j;
-  
-  Serial.println("Hi there!");
-
-  // Apply reverse voltage, charge up the pin and led capacitance
-  pinMode(LED_N_SIDE,OUTPUT);
-  pinMode(LED_P_SIDE,OUTPUT);
-  digitalWrite(LED_N_SIDE,HIGH);
-  digitalWrite(LED_P_SIDE,LOW);
-  
-  Serial.println(digitalRead(LED_N_SIDE));
-  Serial.println(digitalRead(LED_P_SIDE));
-
-  // Isolate the pin 2 end of the diode
-  pinMode(LED_N_SIDE,INPUT);
-  digitalWrite(LED_N_SIDE,LOW);  // turn off internal pull-up resistor
-
-  // Count how long it takes the diode to bleed back down to a logic zero
-  for ( j = 0; j < 30000; j++) {
-    if ( digitalRead(LED_N_SIDE)==0) break;
-  }
-  // You could use 'j' for something useful, but here we are just using the
-  // delay of the counting.  In the dark it counts higher and takes longer, 
-  // increasing the portion of the loop where the LED is off compared to 
-  // the 1000 microseconds where we turn it on.
-
-  // Turn the light on for 1000 microseconds
-  digitalWrite(LED_P_SIDE,HIGH);
-  digitalWrite(LED_N_SIDE,LOW);
-  pinMode(LED_P_SIDE,OUTPUT);
-  pinMode(LED_N_SIDE,OUTPUT);
-  delayMicroseconds(1000);
-  // we could turn it off, but we know that is about to happen at the loop() start
+    delay(1000);
 }
