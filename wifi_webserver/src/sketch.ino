1f58d2b27a769cd609e32a23b1c8facbeef1b7e7
Eleonore <eleonore.mayola@gmail.com>
2014-11-11 13:30:59 +0000

    this issue doesnt exists for the webserver sketch...

--- wifi_webserver/src/sketch.ino
+++ wifi_webserver/src/sketch.ino
@@ -11,8 +11,8 @@ Project using my Arduino Uno, wifi Shield and light sensor
 
 // Global variables
 // for the server
-char ssid[] = "MyNetwork";
-char pass[] = "****";
+char ssid[] = "";
+char pass[] = "";
 int status = WL_IDLE_STATUS;
 WiFiServer server(80);
 
@@ -76,12 +76,22 @@ void loop()
 
         // code to get the sensor data from analog pin A0:
             int sensorReading = analogRead(0);
-            //Serial.println(sensorReading);
-            Rsensor = (float) (1023 - sensorReading) * 10 / sensorReading;
+            
+            // LIGHT SENSOR
+            //Rsensor = (float) (1023 - sensorReading) * 10 / sensorReading;
+            // TEMPERATURE SENSOR
+            
+            // converting that reading to voltage, for 3.3v arduino use 3.3
+            float voltage = sensorReading * 5.0;
+            voltage /= 1024.0; 
+            float temperatureC = (voltage - 0.5) * 100;
+
             delay(100);
             client.print("analog input ");
             client.print(" is ");
-            client.print(Rsensor);
+            //client.print(Rsensor);
+            client.print(temperatureC);
+            client.print(" degrees C");
             client.println("<br />");      
 
         client.println("</html>");
